030d86df7a2aa83318ad222c0d010c06c4ff42e6
nitrokolers19 <nitrokolers19@gmail.com>
2026-02-22 00:45:26 -0600

    Agregado CORS

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const cors = require("cors");
+
 const app = express();
 
+app.use(cors()); //habilita CORS para todas las rutas
 app.use(express.json());
 
 let ultimosAngulos = {
@@ -8,20 +11,17 @@ let ultimosAngulos = {
   a4: 0, a5: 0, a6: 0, a7: 0
 };
 
-// Recibe datos desde la página web
 app.post("/setAngles", (req, res) => {
   ultimosAngulos = req.body;
   console.log("Nuevos ángulos:", ultimosAngulos);
   res.json({ status: "ok" });
 });
 
-// ESP32 obtiene los datos
 app.get("/getAngles", (req, res) => {
   res.json(ultimosAngulos);
 });
 
 const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
   console.log("Servidor corriendo en puerto", PORT);
 });
